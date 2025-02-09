939ff0cfee0cda5a545b9b7a1f9c6e476077c444
Pratik Hotchandani <47892144+PratikHotchandani22@users.noreply.github.com>
2025-02-09 15:57:14 -0500

    Update credentials.py

--- credentials.py
+++ credentials.py
@@ -2,6 +2,6 @@
 
 # OPENAI_API = " "
 
-SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZmZHVkcnR3amt6eWRnZWNoaWdnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzkwNDI3MDYsImV4cCI6MjA1NDYxODcwNn0.bOKCBdLn2EHG3_uJHPEdAq9d-i5EfMeHOe_UfjPf-70"
-SUPABASE_URL =  "https://ffdudrtwjkzydgechigg.supabase.co"
+SUPABASE_KEY = ""
+SUPABASE_URL =  ""
 

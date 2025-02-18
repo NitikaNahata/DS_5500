27d3175bc684b90eb35c43ed4d73a40271691ffb
ProjectKingsNLP <pratikhotchandani22@gmail.com>
2025-02-18 12:39:09 -0500

    getting songs from user's spotify playlist and adding it to supabase

--- credentials.py
+++ credentials.py
@@ -1,3 +1,10 @@
 ## This file will have all the apis keys and secrets.
 
 # OPENAI_API = " "
+
+SUPABASE_KEY = ""
+SUPABASE_URL =  ""
+
+SPOTIFY_CLIENT_ID = ""
+SPOTIFY_CLIENT_SECRET = ""
+REDIRECT_URI = ""
